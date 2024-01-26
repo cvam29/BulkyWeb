8e25737ebd7160ac0632de08c39facd80b1635f2
shivam <shivam@southpawstudio.co>
2024-01-26 19:17:02 +0530

    Setup

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,80 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers;
+public class CategoryController(ApplicationDbContext _context) : Controller
+{
+    public  IActionResult Index()
+    {
+        var categoryList =  _context.Categories.ToList();
+        return View(categoryList);
+    }
+
+    public IActionResult Create() 
+    { 
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Category category)
+    {
+        if(ModelState.IsValid)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            TempData["Success"] = " Catgory Created SuccessFully!";
+             return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public IActionResult Edit(int id)
+    {
+        if(id is 0)
+        {
+            return NotFound();
+        }
+        Category? category =_context.Categories.Find(id);
+        if(category == null)
+        {
+            return NotFound();
+        }
+        return View(category);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            TempData["Success"] = " Catgory Updated SuccessFully!";
+
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public IActionResult Delete(int id)
+    {
+
+        if (id is 0)
+        {
+            return NotFound();
+        }
+        Category? category = _context.Categories.Find(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        _context.Categories.Remove(category);
+        _context.SaveChanges();
+        TempData["Success"] = " Catgory Deleted SuccessFully!";
+
+        return RedirectToAction("Index");
+
+    }
+}
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -2,4 +2,12 @@
 
 public class Category
 {
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [Display( Name = "Category name")]
+    public string Name { get; set; } = string.Empty;
+    [Display(Name = "Display Order")]
+    [Range(1,10, ErrorMessage ="Display Order must be in between 1-100 ")]
+    public int DisplayOrder { get; set; }
 }
